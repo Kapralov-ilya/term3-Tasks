6c1d2beb7e973f8036bbacca70121917b1dc03c0
Kapralov-ilya <73639352+Kapralov-ilya@users.noreply.github.com>
2020-10-29 16:52:01 +0300

    Update task4_2.c

--- task4_2.c
+++ task4_2.c
@@ -19,65 +19,65 @@ void copystring(int sourceFile, int destinationFile)
 {
 	int letter='a',result;
 	while (letter!='\n'){
-        	result=read(sourceFile,&letter,1);
-        	if (result==-1){
-        	    perror("read");
-            	exit(1);
-       		}
-        	result=write(destinationFile,&letter,1);
-        	if (result==-1){
-        	    perror("write");
-        	    exit(1);
-        	}
-    	}
+       		result=read(sourceFile,&letter,1);
+       		if (result==-1){
+       		    perror("read");
+			exit(1);
+		}
+		result=write(destinationFile,&letter,1);
+		if (result==-1){
+			perror("write");
+			exit(1);
+		}
+	}
 }
 
 void sortfile(int sourceFile, int destinationFile, int maxlength)
 {
 	int counter, letter='a', result, currentlength;
 	for(counter=1; counter<=maxlength; counter++){
-    		lseek(sourceFile,0,0);
-        	currentlength=0;
-        	while ((result=read(sourceFile,&letter,1))!=0){
-        		if (result==-1){
-                		perror("read");
-                		exit(1);
-            		}
-            		currentlength++;
-            		if (letter=='\n'){
-                		if (currentlength==counter){
-                			lseek(sourceFile,-counter,1);
-                    			copystring(sourceFile,destinationFile);
-                		}
-                	currentlength=0;
-            		}	
-        	}
-    	}
+		lseek(sourceFile,0,0);
+		currentlength=0;
+		while ((result=read(sourceFile,&letter,1))!=0){
+			if (result==-1){
+				perror("read");
+				exit(1);
+			}
+			currentlength++;
+			if (letter=='\n'){
+				if (currentlength==counter){
+					lseek(sourceFile,-counter,1);
+					copystring(sourceFile,destinationFile);
+				}
+			currentlength=0;
+			}	
+		}
+	}
 }
 
 int main(int argc,char **argv)
 {
 	int sourceFile,destinationFile,maxlength;
 	char *nameFile;
-    	if (argc!=3){
-        	fprintf(stderr,"Wrong amount of arguments\n");
-        	exit(1);
-    	}
-    	sourceFile=open(argv[1],O_RDONLY);
-    	if (sourceFile==-1){
-        	perror(argv[1]);
-        	exit(1);
-    	}
-    	nameFile=generateFile(argv[1]);
-    	maxlength=atoi(argv[2]);
-    	destinationFile=open(nameFile,O_WRONLY|O_TRUNC|O_CREAT,0644);
-    	if (destinationFile==-1){
-    		perror(nameFile);
-        	exit(1);
-    	}
-    	sortfile(sourceFile , destinationFile,maxlength);
-    	close(sourceFile);
-    	close(destinationFile);
-    	free(nameFile);
-   	return 0;
+	if (argc!=3){
+		fprintf(stderr,"Wrong amount of arguments\n");
+		exit(1);
+	}
+	sourceFile=open(argv[1],O_RDONLY);
+	if (sourceFile==-1){
+		perror(argv[1]);
+		exit(1);
+	}
+	nameFile=generateFile(argv[1]);
+	maxlength=atoi(argv[2]);
+	destinationFile=open(nameFile,O_WRONLY|O_TRUNC|O_CREAT,0644);
+	if (destinationFile==-1){
+		perror(nameFile);
+		exit(1);
+	}
+	sortfile(sourceFile , destinationFile,maxlength);
+	close(sourceFile);
+	close(destinationFile);
+	free(nameFile);
+	return 0;
 }
